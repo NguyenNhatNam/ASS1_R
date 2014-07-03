edcc2d3ed266675243eb26f72e7631c1bb15ba51
Nguyen Nhat Nam <nguyennhatnam0509@gmail.com>
2014-07-03 19:58:16 +0700

    Code complete

--- pollutantmean.R
+++ pollutantmean.R
@@ -12,30 +12,47 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
 
         ## Return the mean of the pollutant across all monitors list
         ## in the 'id' vector (ignoring NA values)
-        filenames <-NULL
-        n <-length(id)
-        data list(n)
         
-        for(i in id)
-        {
-                if(i<10)
-                {
-                        
-                        filenames <-paste("00", i,".csv", sep="")
-                        
-                }
-                else if
-                {
-                        filenames <-paste("0", i,".csv", sep="")
-                }
-                else
-                {
-                        filenames <-paste(i,".csv", sep="")
-                }
-                
-                data[i] <-read.csv("specdata/"filenames)
-                
-        }
+#         filenames <-NULL
+#         n <-length(id)
+#         data list(n)
+#         
+#         for(i in id)
+#         {
+#                 if(i<10)
+#                 {
+#                         
+#                         filenames <-paste("00", i,".csv", sep="")
+#                         
+#                 }
+#                 else if
+#                 {
+#                         filenames <-paste("0", i,".csv", sep="")
+#                 }
+#                 else
+#                 {
+#                         filenames <-paste(i,".csv", sep="")
+#                 }
+#                 
+#                 data[i] <-read.csv("specdata/"filenames)
+#                 
+#         }
+        
+        ##liet ke tat ca ten cac file trong thu muc derectory va gan ten vao bien f
+        f <- list.files(directory)
+        ##doc du lieu csv tu cac ten file da chon 
+        dt <-lapply(paste(directory,"/",f[1:length(id)],sep=""), read.csv, header=TRUE)
+        ##dt duoc tao ra cac list cua cac dataframe, su dung rbind de chuyen dt thanh
+        ## dang dataframe
+        data <- do.call( "rbind", dt)
+        ##trich xuat du lieu 
+        target <-data[pollutant]
+        #loai bo gia tri NA
+        target <-target[!is.na(target)]
+        #Tinh mean
+        mean(target)
+        
+        
         
         
 }
