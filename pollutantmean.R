cc169c044a15533f2904d722e6dc8d3c6c9f97a8
Nguyen Nhat Nam <nguyennhatnam0509@gmail.com>
2014-07-03 11:15:55 +0700

    write the code

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,3 +1,4 @@
+
 pollutantmean <- function(directory, pollutant, id = 1:332) {
         ## 'directory' is a character vector of length 1 indicating
         ## the location of the CSV files
@@ -11,4 +12,30 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
 
         ## Return the mean of the pollutant across all monitors list
         ## in the 'id' vector (ignoring NA values)
+        filenames <-NULL
+        n <-length(id)
+        data list(n)
+        
+        for(i in id)
+        {
+                if(i<10)
+                {
+                        
+                        filenames <-paste("00", i,".csv", sep="")
+                        
+                }
+                else if
+                {
+                        filenames <-paste("0", i,".csv", sep="")
+                }
+                else
+                {
+                        filenames <-paste(i,".csv", sep="")
+                }
+                
+                data[i] <-read.csv("specdata/"filenames)
+                
+        }
+        
+        
 }
