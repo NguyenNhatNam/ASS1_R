2255c35ee47cf3b8acb290fc38390f98cab25231
Nguyen Nhat Nam <nguyennhatnam0509@gmail.com>
2014-07-04 22:47:06 +0700

    Code Assignment 1 part 3 complete

--- corr.R
+++ corr.R
@@ -7,6 +7,35 @@ corr <- function(directory, threshold = 0) {
         ## variables) required to compute the correlation between
         ## nitrate and sulfate; the default is 0
 
-        ## Return a numeric vector of correlations\
+        ## Return a numeric vector of correlations
+        
+        
+        ## names of files
+        f <-list.files(directory)
+        ## List of data
+        data <-list(length(f))
+        ##vector result
+        corr <-c()
+        
+        j <-1
+        
+        for(i in 1:length(f))
+        {
+                ##Read file into data[[i]]
+                data[[i]] <-read.csv(paste(directory,"/",f[i],sep=""))
+                ##Remove NA 
+                y <-data[[i]][complete.cases(data[[i]][, 1:ncol(data[[i]])]), ]
+                ## OR using na.omit
+                ## Cal cor if oserver > threshold
+                if(nrow(y) > threshold ) 
+                {
+                        corr[j] <-(cor(y["sulfate"], y["nitrate"]))
+                        j <-j + 1
+                }
+        }
+        
+        ##return corr vector
+        corr
+        
         
 }
