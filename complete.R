46b47c4bbdf464bbfb97ed3efb48d02003ab81c1
Nguyen Nhat Nam <nguyennhatnam0509@gmail.com>
2014-07-04 11:28:28 +0700

    Code for part 2

--- complete.R
+++ complete.R
@@ -12,4 +12,30 @@ complete <- function(directory, id = 1:332) {
         ## ...
         ## where 'id' is the monitor ID number and 'nobs' is the
         ## number of complete cases
+        
+        ## List of data
+        data <-list(length(id))
+        ## names of files
+        f <-list.files(directory)
+        
+        nobs <-c()
+        
+        
+        for(i in 1:length(id))
+        {
+                ##Read file into data[[i]]
+               data[[i]] <-read.csv(paste(directory,"/",f[i],sep=""))
+               ##Remove NA 
+               y <-data[[i]][complete.cases(data[[i]][, 1:ncol(data[[i]])]), ]
+               ## OR using na.omit
+               ##Number of completely observed cases in each data file
+               nobs[i] <-nrow(y) 
+        }
+
+        
+        id <-c(1:length(id))
+        ##Create data frame result
+        result <-data.frame(id, nobs)
+             
+        
 }
